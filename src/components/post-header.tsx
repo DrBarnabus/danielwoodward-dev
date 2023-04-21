32dfa351d130ab7863f5b4691b26e0e48f7028cd
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2023-04-21 21:24:26 +0100

    Add support for images in mdx

--- src/components/post-header.tsx
+++ src/components/post-header.tsx
@@ -12,7 +12,7 @@ export const PostHeader = ({ title, publishedDate, tags }: Props) => {
   return (
     <section className="flex flex-col space-y-2">
       <h1 className="text-2xl font-bold text-zinc-900 drop-shadow-sm dark:text-zinc-50 sm:text-3xl md:text-5xl">
-      <Balancer>{title}</Balancer>
+        <Balancer>{title}</Balancer>
       </h1>
       <PostPublished publishedDate={publishedDate} className="text-base text-zinc-700 dark:text-zinc-200 sm:text-lg" />
       <PostTags tags={tags} />
