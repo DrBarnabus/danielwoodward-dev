673389fa3e692bec121b370b32fa43961d35b14e
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2023-04-22 17:06:29 +0100

    Add medium and twitter links to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,20 +1,21 @@
-import { Copyright, GitHub, LinkedIn } from '~/components/icons';
+import { Copyright, GitHub, LinkedIn, Medium, Twitter } from '~/components/icons';
+
+const socials = [
+  { href: 'https://github.com/DrBarnabus', label: 'My GitHub Profile', icon: GitHub },
+  { href: 'https://medium.com/@drbarnabus', label: 'My Medium Profile', icon: Medium },
+  { href: 'https://twitter.com/drbarnabus', label: 'My Twitter Profile', icon: Twitter },
+  { href: 'https://www.linkedin.com/in/daniel-woodward', label: 'My LinkedIn Profile', icon: LinkedIn }
+] as const;
 
 export const Footer = () => {
   return (
     <div className="relative flex flex-col items-center justify-center space-y-4">
       <div className="max-xs:px-16 flex flex-row flex-wrap justify-center gap-4">
-        <a href="https://github.com/DrBarnabus" target="_blank" rel="noopener noreferrer" aria-label="My GitHub">
-          <GitHub className="h-6 w-6 text-zinc-900 hover:text-accent dark:text-zinc-100 dark:hover:text-accent" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/daniel-woodward/"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="My LinkedIn"
-        >
-          <LinkedIn className="h-6 w-6 text-zinc-900 hover:text-accent dark:text-zinc-100 dark:hover:text-accent" />
-        </a>
+        {socials.map(({href, label, icon }) => (
+          <a key={href} href={href} target="_blank" rel="noopener noreferrer" aria-label={label}>
+            {icon({ className: 'h-6 w-6 text-zinc-900 hover:text-accent dark:text-zinc-100 dark:hover:text-accent' })}
+          </a>
+        ))}
       </div>
 
       <div className="flex h-6 flex-row items-center justify-center space-x-1 text-zinc-600 dark:text-zinc-300">
