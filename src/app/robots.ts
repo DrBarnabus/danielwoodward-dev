1351cf99d27e57e016fa355f03506b81e24acceb
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2024-02-16 21:48:33 +0000

    Replace `next-sitemap` with dynamic sitemap/robots

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/']
+      }
+    ],
+    host: process.env.NEXT_PUBLIC_SITE_URL,
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`
+  }
+}
