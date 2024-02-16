3fd54bdc6f6d4c6e86b06b3c5209c9252f337761
Daniel Woodward <16451892+DrBarnabus@users.noreply.github.com>
2024-02-16 21:48:33 +0000

    Replace `next-sitemap` with dynamic sitemap/robots

---
+++ src/app/sitemap.ts
@@ -0,0 +1,29 @@
+import { allPosts } from 'contentlayer/generated';
+import type { MetadataRoute } from 'next';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = allPosts.sort((a, b) => new Date(b.publishedDate).getTime() - new Date(a.publishedDate).getTime());
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/posts`,
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    ...posts.map((post) => ({
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}${post.url}`,
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    })),
+    {
+      url: `${process.env.NEXT_PUBLIC_SITE_URL}/about`,
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+  ];
+}
